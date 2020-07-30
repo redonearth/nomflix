12a69f26db0d4167b362c4fafab99afa04d7814b
Redonearth <wona23@gmail.com>
2020-07-30 21:45:57 +0900

    #2.1 React Router Part One

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Router from "Components/Router";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <>
+        <Router />
+      </>
+    );
   }
 }
 
