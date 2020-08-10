92fc7d62be9cc70c645c63f965d536e0992831b1
redonearth <wona23@gmail.com>
2020-08-10 13:59:08 +0900

    Fix Loader.js

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
   font-size: 28px;
