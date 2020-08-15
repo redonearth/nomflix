c334836e2d3ec05c8db3404ece6f3cdb43a9e557
redonearth <wona23@gmail.com>
2020-08-15 22:11:58 +0900

    #6.8 Detail Container part One

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,65 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
 
-const DetailPresenter = ({ result, loading, error }) => null;
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 50px);
+  padding: 50px;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  filter: blur(3px);
+  opacity: 0.5;
+  z-index: 0;
+`;
+
+const Content = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`;
+
+const Cover = styled.div`
+  width: 30%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  border-radius: 5px;
+`;
+
+const DetailPresenter = ({ result, loading, error }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <Backdrop
+        bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+      />
+      <Content>
+        <Cover
+          bgImage={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+              : require("../../assets/noPosterSmall.png")
+          }
+        />
+      </Content>
+    </Container>
+  );
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
